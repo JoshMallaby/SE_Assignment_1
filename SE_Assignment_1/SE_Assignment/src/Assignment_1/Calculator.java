da84acad63bcda96f2278004831063e8587c08cc
Riki <asianrikzter@gmail.com>
2020-03-18 15:04:22 +1300

    Junit Test

--- SE_Assignment_1/SE_Assignment/src/Assignment_1/Calculator.java
+++ SE_Assignment_1/SE_Assignment/src/Assignment_1/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
 	public static int add(int a, int b) {
 		return a + b;
 	}
-	public  static int sub(int a, int b) {
+	public static int sub(int a, int b) {
 		return a - b;
 	}
         public static int mult(int a, int b){
